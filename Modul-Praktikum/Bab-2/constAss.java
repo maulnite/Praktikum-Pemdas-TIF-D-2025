3d29b2d497f868df5dad7c097a5e84b256361cc9
HUB37T <hubertcendana@gmail.com>
2025-09-09 18:57:14 +0700

    ab

---
+++ Modul-Praktikum/Bab-2/constAss.java
@@ -0,0 +1,7 @@
+public class constAss{
+    public static void main(String[] args){
+        String s = "filkom";
+        String val = (s=="filkom")?"Brawijaya": "null";
+        System.out.println(s+" "+val);
+    }
+}
