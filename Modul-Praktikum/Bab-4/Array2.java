c38737c136e4ab8b4d317d661aba793eecee9c76
orieabynmlna <orieabyanm@gmail.com>
2025-09-30 14:06:54 +0700

    bab 4

---
+++ Modul-Praktikum/Bab-4/Array2.java
@@ -0,0 +1,28 @@
+public class Array2 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String nama[] = new String[10];
+        int[][] coba = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+        double[][] bank = new double[2][3];
+
+        System.out.println(" i  j coba[i][j]");
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("%2d %2d %3d\n", i, j, coba[i][j]);
+            }
+        }
+
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 0; j < 3; j++) {
+                bank[i][j] = i * 0.5 + j * 0.25;
+            }
+        }
+
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(bank[i][j] + " ");
+            }
+        }
+    }
+}
---
+++ PPT/BAB-3/contoh.java
@@ -0,0 +1,7 @@
+public class contoh {
+    public static void main(String[] args) {
+        for (int i = 0; true; i--) {
+            System.out.println("Hello");
+        }
+    }
+}
